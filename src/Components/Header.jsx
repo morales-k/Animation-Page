8904c318f57908b8c25c10c615e1adb37762697e
Krista M <morales-k@outlook.com>
2023-05-22 22:32:35 -0400

    Created Circle Component
    Added base circle component. Updated CSS boilerplate import.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import Circle from './Circle';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div>
+      <h1>CSS Animations</h1>
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+      eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+      Venenatis lectus magna fringilla urna porttitor rhoncus dolor 
+      purus non.
+      </p>
+      <Circle />
+    </div>
   )
 }
 
