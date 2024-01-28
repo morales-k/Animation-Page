26364f303494f3b7fe9c9a6034e8cb3cd61b802f
Krista M <morales-k@outlook.com>
2024-01-28 13:08:33 -0500

    Add first couple animations

---
+++ src/Animations/DotToPetal/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './index.css';
+
+function DotToPetal() {
+
+  return (
+    <div id="dotContainer">
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+    </div>
+  )
+}
+
+export default DotToPetal;
---
+++ src/Animations/Transfer/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './index.css';
+
+function Transfer() {
+
+  return (
+    <div id="dataContainer">
+      <div className="flex-row">
+        <div className="diamond"></div>
+        <div className="diamond"></div>
+        <div className="diamond"></div>
+      </div>
+      
+       <div className="circle">
+        <div className="diamond"></div>
+       </div>
+       
+       <div className="flex-row">
+        <div className="diamond"></div>
+        <div className="diamond"></div>
+        <div className="diamond"></div>
+      </div>
+    </div>
+  )
+}
+
+export default Transfer;
